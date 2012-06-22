2bf08f8d36aded734467bc56c3bc5087b0f1d5bf
Isaac <isaacwang@gmail.com>
2012-06-22 13:13:54 -0700

    Basic admin template setup

--- meteor/meteor.js
+++ meteor/meteor.js
@@ -2,15 +2,43 @@
 Items = new Meteor.Collection("items");
 Favorites = new Meteor.Collection("favorites");
 
+function is_favorite(id) {
+    return Favorites.findOne({_id: id});
+}
+
 if (Meteor.is_client) {
     Template.items.items = function() {
         return Items.find({}, {sort: {time: -1}});
     };
 
+    Template.admin.items = Template.items.items;
+
+    Template.table_item.favorite = function() {
+        return is_favorite(this._id);
+    };
+
+    Template.admin.events = {
+        'click .add': function() {
+            Items.insert({
+                img_url: "",
+                name: "",
+                owner: "",
+                description: ""
+            });
+        }
+    };
+
+    Template.table_item.events = {
+        'click .delete': function() {
+            console.log(this);
+            //Items.remove({_id: this._id});
+        }
+    }
+
     var MSRouter = Backbone.Router.extend({
         routes: {
             "" : "main",
-            "add" : "add",
+            "admin" : "admin",
             ":id" : "id"
         },
         main: function() {
@@ -20,9 +48,9 @@ if (Meteor.is_client) {
         id: function(id) {
             this.navigate("");
         },
-        add: function() {
+        admin: function() {
             // Redirect to add page
-            Session.set("page_id", "add");
+            Session.set("page_id", "admin");
         }
     });
 
