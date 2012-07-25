0f27f9ab2d812d3bd4afb5cba283a59ee9a8e270
Isaac <isaacwang@gmail.com>
2012-07-25 00:28:16 -0700

    Add 'Request new item' template button in RequestItemView
    The RequestItemView now has a static method that returns an Item
    object with the 'img_url' pointing to a request image.  The image is
    currently pointing to a random image I found online and I still need
    to make an image that says 'Can't find something?  Request an image here'

---
+++ Mustard Seed/Mustard Seed/Views/RequestItemView.h
@@ -0,0 +1,13 @@
+//
+//  RequestItemView.h
+//  Mustard Seed
+//
+//  Created by Isaac Wang on 7/23/12.
+//  Copyright (c) 2012 LMD Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RequestItemView : UIView
+
+@end
---
+++ Mustard Seed/Mustard Seed/Views/RequestItemView.m
@@ -0,0 +1,31 @@
+//
+//  RequestItemView.m
+//  Mustard Seed
+//
+//  Created by Isaac Wang on 7/23/12.
+//  Copyright (c) 2012 LMD Corp. All rights reserved.
+//
+
+#import "RequestItemView.h"
+
+@implementation RequestItemView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
